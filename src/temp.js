cf89c2370dc5c72a6d8521cc4bcc46fd4957884b
NagendraBabuG <gnagendra0009@gmail.com>
2023-05-05 17:25:07 +0530

    initial commit

--- src/temp.js
+++ src/temp.js
@@ -1,58 +1,38 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./App.css";
-import { useState } from "react";
-
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
+
 function App() {
   const [Msg, setMsg] = useState("Upload Xlsx files here");
-  const [selectedFile, setSelectedFile] = useState(undefined)
   const fileInputRef = useRef(null);
-  
+  let selectedFile = undefined;
+
   const handleFileUpload = async (event) => {
-    const tmp = event.target.files[0]
-    setSelectedFile(event.target.files[0]);
-   // console.log(selectedFile);
-   console.log(tmp)
-    if(event.target.files[0] === undefined) return;
-    setMsg(tmp.name);
-    console.log(Msg)
+    selectedFile = event.target.files[0];
+    console.log(selectedFile);
+    setMsg(selectedFile.name);
   };
+
   const onSubmit = async () => {
     const formData = new FormData();
-    console.log('selected file ,' , selectedFile)
-    if(selectedFile === undefined) {
-      //console.log(selectedFile) 
-      setMsg('Error')
-       return;
-    }
     formData.append("file", selectedFile);
-    try{
-      const config = {
-        method: "post",
-        maxBodyLength: Infinity,
-        url: "https://excelupload-server.onrender.com/upload",
-        headers: {
-          "Content-Type": formData.get('Content-Type')
-        },
-        data: formData,
-      };
-
-      const resp = await axios.request(config)
-      console.log(resp)
-    }
-    catch(error)
-    {
-      console.log(error)
-      setMsg('Error')
+    try {
+      const response = await axios.post(
+        "https://example.com/api/upload",
+        formData
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
     }
   };
 
   const handleClick = () => {
     fileInputRef.current.click();
   };
+
   return (
     <div className="App">
       <div className="heading">Add Candidates to Database</div>
@@ -68,7 +48,9 @@ function App() {
           <FontAwesomeIcon icon={faUpload} size="2xl"></FontAwesomeIcon>
         </button>
         <br></br>
-        <button className="submit" onClick={onSubmit}  style={{ display: selectedFile ? "inline-block" : "none" }}>Submit</button>
+        <button className="submit" onClick={onSubmit}>
+          Submit
+        </button>
       </div>
       <div>{Msg}</div>
     </div>

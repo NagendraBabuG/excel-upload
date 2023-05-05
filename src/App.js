93bda8ba8fbb4f34353a97799ea3d3f819225214
NagendraBabuG <gnagendra0009@gmail.com>
2023-05-05 21:02:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,60 +3,71 @@ import "./App.css";
 import { useState } from "react";
 
 import axios from "axios";
+import Response from "./Response";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
+
 function App() {
-  const [Msg, setMsg] = useState("Upload Xlsx files here");
-  const [selectedFile, setSelectedFile] = useState(undefined)
+  const [msg, setMsg] = useState("Upload Xlsx files here");
+  const [selectedFile, setSelectedFile] = useState(undefined);
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [respond, setRespond] = useState(400)
   const fileInputRef = useRef(null);
-  
+
   const handleFileUpload = async (event) => {
-    const tmp = event.target.files[0]
+    const tmp = event.target.files[0];
     setSelectedFile(event.target.files[0]);
-   // console.log(selectedFile);
-   console.log(tmp)
-    if(event.target.files[0] === undefined) return;
+    console.log(tmp);
+    
+    if (event.target.files[0] === undefined) return;
     setMsg(tmp.name);
-    console.log(Msg)
   };
+
   const onSubmit = async () => {
     const formData = new FormData();
-    console.log('selected file ,' , selectedFile)
-    if(selectedFile === undefined) {
-      //console.log(selectedFile) 
-      setMsg('Error')
-       return;
+    console.log("selected file ,", selectedFile);
+    if (selectedFile === undefined) {
+      setMsg("Error");
+      return;
     }
     formData.append("file", selectedFile);
-    try{
-      const config = {
-        method: "post",
-        maxBodyLength: Infinity,
-        url: "https://excelupload-server.onrender.com/upload",
-        headers: {
-          "Content-Type": formData.get('Content-Type')
-        },
-        data: formData,
-      };
+    try {
+        const config = {
+            method: "post",
+            maxBodyLength: Infinity,
+            url: "https://excelupload-server.onrender.com/upload",
+            headers: {
+              "Content-Type": formData.get('Content-Type')
+            },
+            data: formData,
+          };
 
-      const resp = await axios.request(config)
-      console.log(resp)
-    }
-    catch(error)
-    {
-      console.log(error)
-      setMsg('Error')
+      const resp = await axios.request(config);
+      setRespond(resp.status)
+      console.log(resp.status)
+      setIsUploaded(true)
+      console.log(resp);
+    } catch (error) {
+      console.log(error);
+      setMsg("Error");
+      setRespond(400)
+      setIsUploaded(true)
     }
   };
 
   const handleClick = () => {
     fileInputRef.current.click();
   };
+
   return (
     <div className="App">
+      <div className="navbar"><h3>Add From Excel</h3></div>
       <div className="heading">Add Candidates to Database</div>
+      
       <div className="container">
+      { isUploaded ? <Response status = {respond}></Response>: <div>
+
         <input
           type="file"
           ref={fileInputRef}
@@ -68,9 +79,12 @@ function App() {
           <FontAwesomeIcon icon={faUpload} size="2xl"></FontAwesomeIcon>
         </button>
         <br></br>
-        <button className="submit" onClick={onSubmit}  style={{ display: selectedFile ? "inline-block" : "none" }}>Submit</button>
+        <button className="submit" onClick={onSubmit} style={{ display: selectedFile ? "inline-block" : "none" }}>
+          Submit
+        </button>
+       </div>}
       </div>
-      <div>{Msg}</div>
+     {!isUploaded && <div>{msg}</div>}
     </div>
   );
 }
